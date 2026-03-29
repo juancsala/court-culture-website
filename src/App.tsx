8e081014fc0838f4c77da8a29728be23350ef305
Juan Sala <jsalapulido@gmail.com>
2026-03-28 21:18:02 -0600

    Revert "Add events section, event detail page, and Stripe checkout flow"
    This reverts commit 148093226de1c5ff03b460b799e3c2e1c59a8f96.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import EventDetail from './pages/EventDetail'
-import Confirmacion from './pages/Confirmacion'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Concept from './components/Concept'
+import HowItWorks from './components/HowItWorks'
+import Community from './components/Community'
+import Join from './components/Join'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/eventos/:id" element={<EventDetail />} />
-        <Route path="/confirmacion" element={<Confirmacion />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="bg-cc-base text-cc-text overflow-x-hidden">
+      <Navbar />
+      <Hero />
+      <Concept />
+      <HowItWorks />
+      <Community />
+      <Join />
+      <Footer />
+    </div>
   )
 }
